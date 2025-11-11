733264bf889e6252862f9bf107aa636451369758
AntonyNeal <antony@life-psychology.com.au>
2025-11-11 16:18:59 +1100

    Remove TenantProvider - convert to standalone single-tenant site

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
-import { TenantProvider } from './core/providers/TenantProvider';
 import App from './App';
 import './index.css';
 
@@ -20,9 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <HelmetProvider>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <TenantProvider>
-          <App />
-        </TenantProvider>
+        <App />
       </BrowserRouter>
     </QueryClientProvider>
   </HelmetProvider>
